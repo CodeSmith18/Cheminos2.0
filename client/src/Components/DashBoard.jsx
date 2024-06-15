3deb3036fc165980a067ab06cf2736007e53b816
Ritik Raj <ritikraj1875@gmail.com>
2024-06-15 16:07:43 +0530

    somechahes

--- client/src/Components/DashBoard.jsx
+++ client/src/Components/DashBoard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./dashboard.css";
 import Dashcard from "./DashboardLinls/Dashcard";
 
-const renderFrom = [1, 2, 3, 4, 5, 6];
+const renderFrom = [1, 2, 3, 4, 5, 6,7,8];
 
 const Dashboard = () => {
 	return (
